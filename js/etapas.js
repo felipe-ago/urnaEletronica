9ea8b98ad113a0973830ac9f9a1fe034e12f3ac4
Felipe Oliveira <lipe111094@hotmail.com>
2022-11-04 15:46:17 -0300

    creating candidate database

---
+++ js/etapas.js
@@ -0,0 +1,201 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numero: 4,
+        candidatos: [
+            {
+                numero: 1010,
+                nome: 'Bulbassaur',
+                partido: 'PT - Partido Terrestre',
+                fotos: [
+                    { url: '/image/Deputado Federal/Bulbassaur.png', legenda: 'Deputado Federal' }
+                ]
+            },
+            {
+                numero: 2020,
+                nome: 'Charmander',
+                partido: 'PF - Partido do Fogo',
+                fotos: [
+                    { url: '/image/Deputado Federal/Charmander.png', legenda: 'Deputado Federal' }
+                ]
+            },
+            {
+                numero: 3030,
+                nome: 'Pidgey',
+                partido: 'PV - Partido da Ventania',
+                fotos: [
+                    { url: '/image/Deputado Federal/Pidgey.png', legenda: 'Deputado Federal' }
+                ]
+            },
+            {
+                numero: 4040,
+                nome: 'Squirtle',
+                partido: 'PA - Partido Aquático',
+                fotos: [
+                    { url: '/image/Deputado Federal/Squirtle.png', legenda: 'Deputado Federal' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numero: 5,
+        candidatos: [
+            {
+                numero: 10010,
+                nome: 'Crocodile',
+                partido: 'PT - Partido Terrestre',
+                fotos: [
+                    { url: '/image/Deputado Estadual/Crocodile.png', legenda: 'Deputado Estadual' }
+                ]
+            },
+            {
+                numero: 20020,
+                nome: 'Ace',
+                partido: 'PF - Partido do Fogo',
+                fotos: [
+                    { url: '/image/Deputado Estadual/Ace.png', legenda: 'Deputado Estadual' }
+                ]
+            },
+            {
+                numero: 30030,
+                nome: 'Smoker',
+                partido: 'PV - Partido da Ventania',
+                fotos: [
+                    { url: '/image/Deputado Estadual/Smoker.png', legenda: 'Deputado Estadual' }
+                ]
+            },
+            {
+                numero: 40040,
+                nome: 'Jinbei',
+                partido: 'PA - Partido Aquático',
+                fotos: [
+                    { url: '/image/Deputado Estadual/Jinbei.png', legenda: 'Deputado Estadual' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'SENADOR',
+        numero: 3,
+        candidatos: [
+            {
+                numero: 100,
+                nome: 'Gaara',
+                partido: 'PT - Partido Terrestre',
+                fotos: [
+                    { url: '/image/Senador/Gaara.png', legenda: 'Senador' }
+                ]
+            },
+            {
+                numero: 200,
+                nome: 'Konohamaru',
+                partido: 'PF - Partido do Fogo',
+                fotos: [
+                    { url: '/image/Senador/Konohamaru.png', legenda: 'Senador' }
+                ]
+            },
+            {
+                numero: 300,
+                nome: 'Temari',
+                partido: 'PV - Partido da Ventania',
+                fotos: [
+                    { url: '/image/Senador/Temari.png', legenda: 'Senador' }
+                ]
+            },
+            {
+                numero: 400,
+                nome: 'Suigetsu',
+                partido: 'PA - Partido Aquático',
+                fotos: [
+                    { url: '/image/Senador/Suigetsu.png', legenda: 'Senador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'GOVERNADOR',
+        numero: 2,
+        candidatos: [
+            {
+                numero: 10,
+                nome: 'Toph',
+                partido: 'PT - Partido Terrestre',
+                fotos: [
+                    { url: '/image/Governador/Toph.png', legenda: 'Governador' }
+                ]
+            },
+            {
+                numero: 20,
+                nome: 'Zuko',
+                partido: 'PF - Partido do Fogo',
+                fotos: [
+                    { url: '/image/Governador/Zuko.png', legenda: 'Governador' }
+                ]
+            },
+            {
+                numero: 30,
+                nome: 'Aang',
+                partido: 'PV - Partido da Ventania',
+                fotos: [
+                    { url: '/image/Governador/Aang.png', legenda: 'Governador' }
+                ]
+            },
+            {
+                numero: 40,
+                nome: 'Katara',
+                partido: 'PA - Partido Aquático',
+                fotos: [
+                    { url: '/image/Governador/Katara.png', legenda: 'Governador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numero: 2,
+        candidatos: [
+            {
+                numero: 10,
+                nome: 'Floresta',
+                partido: 'PT - Partido Terrestre',
+                vice: 'Árvore',
+                fotos: [
+                    { url: '/image/Presidente/Floresta.png', legenda: 'Presidente' },
+                    { url: '/image/Presidente/Arvore.png', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+            {
+                numero: 20,
+                nome: 'Vulcão',
+                partido: 'PF - Partido do Fogo',
+                vice: 'Lava',
+                fotos: [
+                    { url: '/image/Presidente/Vulcao.png', legenda: 'Presidente' },
+                    { url: '/image/Presidente/Lava.png', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+            {
+                numero: 30,
+                nome: 'Furacão',
+                partido: 'PV - Partido da Ventania',
+                vice: 'Vento',
+                fotos: [
+                    { url: '/image/Presidente/Furacao.png', legenda: 'Presidente' },
+                    { url: '/image/Presidente/Vento.png', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+            {
+                numero: 40,
+                nome: 'Praia',
+                partido: 'PA - Partido Aquático',
+                vice: 'Onda',
+                fotos: [
+                    { url: '/image/Presidente/Praia.png', legenda: 'Presidente' },
+                    { url: '/image/Presidente/Onda.png', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+        ]
+    }
+
+];
